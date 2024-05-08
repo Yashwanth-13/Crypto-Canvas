edab3adbd8e33c4966a3a499651d5ad1c91e52c6
THIPPANA VIVEKANANDA REDDY <vivekanandareddy56@gmail.com>
2024-05-09 00:11:49 +0530

    pop

--- renderer.js
+++ renderer.js
@@ -5,21 +5,30 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+
+let mouseup=true;
+let mousedown=false;
+let mousenowx=0,mousenowy=0;
+let x=0,y=0;
+
 let imgInp = document.getElementById("imgInp")
-let blah = document.getElementById("blah")
 let canvas= document.getElementById("canvas")
 let ctx= canvas.getContext("2d")
-let img = new Image()
+let img = new Image(),imgpop = new Image();
 let width1=0,height1=0
 let width=0,height=0
+let popwidth=0,popheight=0
 let ratio=0;
 let sactive=false
+let change=false
 canvas.style.background = "black";
 sactive=false
 
 let blur=0,brightness=100,contrast=100,grayscale=0,hueRotate=0,invert=0,opacity=0,saturate=100,sepia=0;
-if(localStorage.getItem("blur")>0){
-    console.log("ok ");
+change=localStorage.getItem("change");
+if(change){
+    //console.log("ok ");
     blur=localStorage.getItem("blur");
     brightness=localStorage.getItem("brightness");
     contrast=localStorage.getItem("contrast");
@@ -32,10 +41,12 @@ if(localStorage.getItem("blur")>0){
 }
 loadfilter();
 
+change=false
+
 let source=localStorage.getItem("image")
-console.log(source)
+//console.log(source)
 if(source){
-    console.log(sactive);
+    //console.log(sactive);
     img.src = source;
     img.onload = function () {
         load()
@@ -44,36 +55,36 @@ if(source){
 
 function bring(){
     let [file] = imgInp.files
-    console.log("change")
+    // console.log(document.getElementById("imgInp").files[0].path)
     if (file) {
-        console.log("file")
-        blah.src = file.path;
-        img.src = blah.src;
-        console.log(img.src);
+        //console.log("file")
+        img.src = document.getElementById("imgInp").files[0].path;
+        //console.log(img.src);
         img.onload = function () {
-            load()
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            load(img)
         }
     }
 }
 function load(){
     width = img.naturalWidth;
     height = img.naturalHeight;
-    console.log(width,height);
+    //console.log(width,height);
     if(height!=600 && width!=800){
         height1=height
         width1=width
         ratio=width/height
         if(ratio>1){
-            width=800
-            height=800/ratio
+            width=800;
+            height=800/ratio;
             ctx.drawImage(img, 0, 0,width1,height1,0,0,width,height);
-            console.log(width,height)
+            //console.log(width,height)
         }
         else{
             height=600
             width=600*ratio
             ctx.drawImage(img, 0, 0,width1,height1,0,0,width,height);
-            console.log(width,height)
+            //console.log(width,height)
         }
     }
 }
@@ -83,7 +94,7 @@ function download() {
     canvas.height = img.naturalHeight;
     let ctx = canvas.getContext("2d");
     ctx.filter = "blur("+blur+"px)"+"brightness("+brightness+"%)"+"contrast("+contrast+"%)"+"grayscale("+grayscale+"%)"+"invert("+invert+"%)"+ "hue-rotate("+hueRotate+"deg)"+"saturate("+saturate+"%)"+"sepia("+sepia+"%)";
-    console.log(blur,brightness,contrast,grayscale,hueRotate,invert,opacity,saturate,sepia);
+    //console.log(blur,brightness,contrast,grayscale,hueRotate,invert,opacity,saturate,sepia);
     ctx.drawImage(img, 0, 0);
     let a = document.createElement("a");
     a.href = canvas.toDataURL();
@@ -98,15 +109,6 @@ function applyFilter(){
 function cropImage(){
     sactive=true;
     window.location.href='Crop.html';
-}
-function crop(){
-
-}
-function rotate(){
-
-}
-function mirror(){
-
 }
 function save(){
     sactive=true;
@@ -125,19 +127,36 @@ function text(){
     sactive=true;
     window.location.href='Text.html';
 }
-function handleFileSelect(event) {
-    const file = event.target.files[0];
+function secondpic(){
+    let [file] = imgInp.files
     if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const preview = document.getElementById('preview');
-            preview.innerHTML = `<img src="${e.target.result}" alt="Selected Image" style="max-width: 100%; max-height: 200px;">`;
-        };
-        reader.readAsDataURL(file);
+        imgpop.src = document.getElementById("imgpop").files[0].path;
+        imgpop.onload = function () {
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            load(); 
+            popwidth = imgpop.naturalWidth;
+            popheight = imgpop.naturalHeight;
+            //console.log(popwidth,popheight);
+            if(popheight!=600 && popwidth!=800){
+                height1=popheight
+                width1=popwidth
+                ratio=popwidth/popheight
+                if(ratio>1){
+                    popwidth=800;
+                    popheight=800/ratio;
+                    ctx.drawImage(imgpop, 0, 0,width1,height1,x,y,popwidth,popheight);
+                    //console.log(popwidth,popheight)
+                }
+                else{
+                    popheight=600
+                    popwidth=600*ratio
+                    ctx.drawImage(imgpop, 0, 0,width1,height1,x,y,popwidth,popheight);
+                    //console.log(popwidth,popheight)
+                }
+            }
+        }
     }
-}
-function submitText(){
-    
+
 }
 
 function blur1(){
@@ -178,9 +197,10 @@ function loadfilter(){
     localStorage.setItem("grayscale",grayscale);
     localStorage.setItem("contrast",contrast);
     localStorage.setItem("brightness",brightness);
+    localStorage.setItem("change",true);
     ctx.filter = "blur("+blur+"px)"+"brightness("+brightness+"%)"+"contrast("+contrast+"%)"+"grayscale("+grayscale+"%)"+"invert("+invert+"%)"+ "hue-rotate("+hueRotate+"deg)"+"saturate("+saturate+"%)"+"sepia("+sepia+"%)";
     ctx.drawImage(img, 0, 0,width,height);
-    console.log(ctx.filter)
+    //console.log(ctx.filter)
 }
 function reset(){
     blur=0,brightness=100,contrast=100,grayscale=0,hueRotate=0,invert=0,opacity=0,saturate=100,sepia=0;
@@ -201,17 +221,45 @@ function getMousePosition(canvas, event) {
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
-    console.log("Coordinate x: " + x,
-        "Coordinate y: " + y);
+    return [x,y];
 }
 
 canvas.addEventListener("mouseup", function (e) {
-    getMousePosition(canvas, e);
+    mouseup=true;
+    mousedown=false;
 }); 
 
 canvas.addEventListener("mousedown", function (e) {
-    getMousePosition(canvas, e);
+    mouseup=false;
+    mousedown=true;
 }); 
+let v1=false;
+let pervmousex=mousenowx,pervmousey=mousenowy;
+canvas.addEventListener("mousemove", function (e) {
+    if(mousedown){
+        [mousenowx,mousenowy]=getMousePosition(canvas, e);
+        if(mousenowx<x+popwidth && mousenowy<y+popheight && mousenowx>x && mousenowy>y){
+            //the error is due to the diff btw mousenow and prevmouse changing if mousedown becomes false and true again
+            if(v1===true){
+                pervmousex=mousenowx;
+                pervmousey=mousenowy;
+                v1=false;
+            }
+            console.log(mousenowx,mousenowy,x,y,popwidth,popheight)
+            x+=mousenowx-pervmousex;
+            y+=mousenowy-pervmousey;
+            console.log(x,y)
+            secondpic();
+            pervmousex=mousenowx;
+            pervmousey=mousenowy;
+            
+        }
+    }
+    else{
+        v1=true
+    }
+});
+
 
 window.onbeforeunload = function() { 
     if(sactive===false)
