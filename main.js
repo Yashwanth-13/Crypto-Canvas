514be19b4c827ff4026622847df6f0ca1a184937
Srimani Teja Ejjagiri <123293913+SrimaniTeja@users.noreply.github.com>
2024-05-29 22:00:06 +0530

    changes
    text changes

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function createWindow () {
 
   
   const mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
